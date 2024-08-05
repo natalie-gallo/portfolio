d21029bacdd3cccf9013c3c90f962d75598cec1e
Natalie Gallo <nataliegallo04@gmail.com>
2024-08-04 22:46:47 -0400

    edited about

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
       <p>My name is Natalie Gallo and I'm a rising Junior studying Computer Science at CUNY Hunter College.</p>
       <p>I was born and raised in The Bronx, New York. I am 20 years old.</p>
       <p>I am a first generation Latina and I am the first in my family to attend college.</p>
-      <p>I like baking, reading, and playing dress to impress with my baddies.</p>
+      <p>I like reading, dancing, and having game nights with my friends</p>
       <p><b>Fun Fact: </b>I am an Aries and I have a dog named Aries who is actually a Sagittarius :p </p>
     </div>
   )
